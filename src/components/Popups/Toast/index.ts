c190974ff1aafdaaeef3e90c12764071550d49f8
Marcin-Migdal <mortimor132@gmail.com>
2024-09-18 23:38:25 +0200

    type returned

--- src/components/Popups/Toast/index.ts
+++ src/components/Popups/Toast/index.ts
@@ -1,5 +1,5 @@
 export * from "./components/icons";
 export { default as ToastsContainer } from "./ToastsContainer";
 
-export { AddToastPayload, CloseIconProps, ToastConfig, ToastHandler, ToastsContainerProps, ToastsPosition } from "./types";
+export { AddToastPayload, CloseIconProps, ToastConfig, ToastHandler, ToastVariant, ToastsContainerProps, ToastsPosition } from "./types";
 export * from "./utils";
