4af48209878da7b3913c3292a5caed0c6aebea5c
Migdal, Marcin ext <m.migdal@xberry.tech>
2026-02-15 11:41:27 +0100

    Resolving pr comments

--- src/hooks/useBreakpoint.ts
+++ src/hooks/useBreakpoint.ts
@@ -5,9 +5,9 @@ type BreakpointKey = "sm" | "md" | "lg" | "xl";
 type BreakpointMap = Record<BreakpointKey, boolean>;
 
 const queries: Record<BreakpointKey, string> = {
-  sm: "(max-width: 768px)",
-  md: "(min-width: 768px) and (max-width: 992px)",
-  lg: "(min-width: 992px) and (max-width: 1200px)",
+  sm: "(max-width: 767.98px)",
+  md: "(min-width: 768px) and (max-width: 991.98px)",
+  lg: "(min-width: 992px) and (max-width: 1199.98px)",
   xl: "(min-width: 1200px)",
 };
 
